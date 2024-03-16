986d6e341950eff75f45488ce79f6abf15766a63
Minji Kim <minjikim2000@hotmail.com>
2024-03-16 18:14:55 +0900

    feat: add conditional rendering to header

--- src/components/common/layout/Header.jsx
+++ src/components/common/layout/Header.jsx
@@ -13,6 +13,28 @@ const HeaderContainer = styled.div`
   margin-top: 2.8rem;
 `;
 
+const LeftContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: end;
+`;
+
+const Text = styled.div`
+  font-family: 'SUITE-Light', sans-serif;
+  font-size: 1.5rem;
+`;
+
+const Icon = styled.button`
+  font-size: 2rem;
+  font-family: 'Material Symbols Outlined', sans-serif;
+  color: ${(props) => props.theme.colors.green};
+  cursor: pointer;
+  padding-left: 0.5rem;
+  border: 0;
+  background: transparent;
+`;
+
 const PointContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -43,37 +65,75 @@ const IconWrapper = styled.div`
   cursor: default;
 `;
 
-const Icon = styled.button`
-  font-size: 2rem;
-  font-family: 'Material Symbols Outlined', sans-serif;
-  color: ${(props) => props.theme.colors.green};
-  cursor: pointer;
-  padding-left: 0.7rem;
-
-  border: 0;
-  background: transparent;
-`;
-
 // Header component
 const Header = () => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate('/store');
-    console.log('success');
+  const handleClickBack = () => {
+    navigate(-1); //이전 페이지로 이동
   };
 
-  return (
-    <HeaderContainer>
-      <PointContainer>
-        <Icon>
-          <IconWrapper>stars</IconWrapper>
-        </Icon>
-        <Point>+70</Point> {/*API 적용 전*/}
-      </PointContainer>
-      <Icon onClick={handleClick}>storefront</Icon>
-    </HeaderContainer>
-  );
+  const handleClickStore = () => {
+    navigate('/store');
+  };
+
+  const determineHeader = () => {
+    if (location.pathname === '/records' || location.pathname === '/매일의미션페이지')
+      return (
+        <HeaderContainer>
+          <LeftContainer>
+            <Icon onClick={handleClickBack}>arrow_back_ios</Icon>
+            <Text>Own Earth</Text>
+          </LeftContainer>
+        </HeaderContainer>
+      );
+    else if (location.pathname === '/home')
+      return (
+        <HeaderContainer>
+          <PointContainer>
+            <Icon>
+              <IconWrapper>stars</IconWrapper>
+            </Icon>
+            <Point>+70</Point> {/*API 적용 전*/}
+          </PointContainer>
+          <Icon onClick={handleClickStore}>storefront</Icon>
+        </HeaderContainer>
+      );
+    else if (location.pathname === '/friends')
+      return (
+        <HeaderContainer>
+          <PointContainer>
+            <Icon>
+              <IconWrapper>stars</IconWrapper>
+            </Icon>
+            <Point>+70</Point> {/*API 적용 전*/}
+          </PointContainer>
+          <Icon onClick={handleClickStore}>group_add</Icon>
+        </HeaderContainer>
+      );
+    else if (location.pathname === '/mypage')
+      return (
+        <HeaderContainer>
+          <LeftContainer>
+            <Icon>account_circle</Icon>
+            <Text>마이페이지</Text>
+          </LeftContainer>
+          <Icon onClick={handleClickStore}>storefront</Icon>
+        </HeaderContainer>
+      );
+    else if (location.pathname === '/친구목록')
+      return (
+        <HeaderContainer>
+          <LeftContainer>
+            <Icon>group</Icon>
+            <Text>친구 목록</Text>
+          </LeftContainer>
+          <Icon>search</Icon>
+        </HeaderContainer>
+      );
+  };
+
+  return <>{determineHeader()}</>;
 };
 
 export default Header;
