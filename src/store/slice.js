1fba26e6e00e859b8dafb2bdd5444aaf76c2b44a
devjaeyoon <devjaeyoon@gmail.com>
2024-03-23 03:29:56 +0900

    chore: install redux related packages and settings

---
+++ src/store/slice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const { actions, reducer } = createSlice({
+  name: 'Own-Earth',
+  initialState: {},
+});
+
+export const {} = actions;
+
+export default reducer;
