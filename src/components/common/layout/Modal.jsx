0d41f0f3f324c3d757b890ee9befbc391110d402
Minji Kim <minjikim2000@hotmail.com>
2024-03-23 15:58:19 +0900

    feat: add myroom modal layout (#28)

--- src/components/common/layout/Modal.jsx
+++ src/components/common/layout/Modal.jsx
@@ -1,19 +1,18 @@
 import PropTypes from 'prop-types';
 import StyledModal from 'react-modal';
 
-const Modal = ({ isOpen, onRequestClose, children }) => {
+const Modal = ({ children, isOpen, onRequestClose, top }) => {
   const customStyles = {
     content: {
       position: 'relative',
       top: '30%',
+      left: '0%',
       right: '0%',
-      left: '10%',
-      width: '270px',
-      height: '168px',
+      margin: '0 3.5rem',
       border: 'none',
-      borderRadius: '20px',
+      borderRadius: '0.9rem',
       backgroundColor: 'white',
-      boxShadow: '2px 2px 2px 2px rgba(0, 0, 0, 0.2)',
+      boxShadow: '2px 2px 12.3px 0px rgba(0, 0, 0, 0.2)',
     },
   };
 
@@ -29,9 +28,30 @@ const Modal = ({ isOpen, onRequestClose, children }) => {
       height: '400px',
       padding: '0',
       border: 'none',
-      borderRadius: '24px',
+      borderRadius: '0.8rem',
       backgroundColor: '#00AA8B66',
-      boxShadow: '2px 2px 2px 2px rgba(0, 0, 0, 0.2)',
+      boxShadow: '2px 2px 12.3px 0px rgba(0, 0, 0, 0.2)',
+    },
+  };
+
+  const myRoomStyles = {
+    //마이룸 페이지 - 마이룸 아이템 모달 레이아웃
+    overlay: {
+      backgroundColor: 'transparent',
+    },
+    content: {
+      overflow: 'scroll',
+      position: 'relative',
+      top: '60%',
+      right: '0%',
+      left: '5%',
+      width: '350px',
+      height: '400px',
+      padding: '0 0 2rem 0',
+      border: 'none',
+      borderRadius: '0.8rem',
+      backgroundColor: '#FFFFFF',
+      boxShadow: '0px 4px 12.3px 0px rgba(0, 0, 0, 0.35)',
     },
   };
 
@@ -39,7 +59,15 @@ const Modal = ({ isOpen, onRequestClose, children }) => {
     <StyledModal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-      style={location.pathname === '/friends-profile' ? friendItemStyles : customStyles}
+      appElement={document.getElementById('root')}
+      style={
+        location.pathname === '/friends-profile'
+          ? friendItemStyles
+          : location.pathname === '/myroom'
+          ? myRoomStyles
+          : customStyles
+      }
+      top={top}
     >
       {children}
     </StyledModal>
@@ -50,6 +78,7 @@ Modal.propTypes = {
   children: PropTypes.node.isRequired,
   isOpen: PropTypes.bool,
   onRequestClose: PropTypes.func,
+  top: PropTypes.string,
 };
 
 export default Modal;
