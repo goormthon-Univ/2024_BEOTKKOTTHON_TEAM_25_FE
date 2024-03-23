e874b6d989a5ec5ad8087cdd34a377fcb42e48fe
Minji Kim <minjikim2000@hotmail.com>
2024-03-23 19:41:17 +0900

    feat: add store related pages (#30)

--- src/pages/StorePage.jsx
+++ src/pages/StorePage.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 import { Header, Footer } from '../components/common/layout';
@@ -7,48 +8,57 @@ import ScreenBackground from '../assets/img/ScreenBackground.png';
 const StorePage = () => {
   const naviagte = useNavigate();
 
+  const point = useSelector((state) => state.point);
+
   return (
-    <Layout>
+    <>
       <Header />
-      <SubTopBar>
-        <RightContainer>
-          <PointContainer>
-            <Icon>
-              <IconWrapper>stars</IconWrapper>
-            </Icon>
-            <Point>70</Point> {/*API 적용 전*/}
-          </PointContainer>
-        </RightContainer>
-      </SubTopBar>
-      <CategoryList>
-        <CategoryItem onClick={() => naviagte('/store/0')}>
-          <ClothIcon />
-          <CategoryItemText>의류</CategoryItemText>
-          <Move />
-        </CategoryItem>
-        <CategoryItem onClick={() => naviagte('/store/1')}>
-          <HangerIcon />
-          <CategoryItemText>액세서리</CategoryItemText>
-          <Move />
-        </CategoryItem>
-        <CategoryItem onClick={() => naviagte('/store/2')}>
-          <FaceIcon />
-          <CategoryItemText>소품</CategoryItemText>
-          <Move />
-        </CategoryItem>
-        <CategoryItem onClick={() => naviagte('/store/3')}>
-          <WallIcon />
-          <CategoryItemText>바닥재</CategoryItemText>
-          <Move />
-        </CategoryItem>
-        <CategoryItem onClick={() => naviagte('/store/4')}>
-          <FloorIcon />
-          <CategoryItemText>벽지</CategoryItemText>
-          <Move />
-        </CategoryItem>
-      </CategoryList>
+      <Layout>
+        <SubTopBar>
+          <RightContainer>
+            <PointContainer>
+              <Icon>
+                <IconWrapper>stars</IconWrapper>
+              </Icon>
+              <Point>{point}</Point> {/*API 적용 전*/}
+            </PointContainer>
+          </RightContainer>
+        </SubTopBar>
+        <CategoryList>
+          <CategoryItem onClick={() => naviagte('/store/0')}>
+            <CategoryIcon>apparel</CategoryIcon>
+            <CategoryItemText>의류</CategoryItemText>
+            <Move />
+          </CategoryItem>
+          <CategoryItem onClick={() => naviagte('/store/1')}>
+            <CategoryIcon>checkroom</CategoryIcon>
+            <CategoryItemText>왼쪽 위 소품</CategoryItemText>
+            <Move />
+          </CategoryItem>
+          <CategoryItem onClick={() => naviagte('/store/2')}>
+            <CategoryIcon>checkroom</CategoryIcon>
+            <CategoryItemText>왼쪽 아래 소품</CategoryItemText>
+            <Move />
+          </CategoryItem>
+          <CategoryItem onClick={() => naviagte('/store/3')}>
+            <CategoryIcon>checkroom</CategoryIcon>
+            <CategoryItemText>오른쪽 위 소품</CategoryItemText>
+            <Move />
+          </CategoryItem>
+          <CategoryItem onClick={() => naviagte('/store/4')}>
+            <CategoryIcon>checkroom</CategoryIcon>
+            <CategoryItemText>오른쪽 아래 소품</CategoryItemText>
+            <Move />
+          </CategoryItem>
+          <CategoryItem onClick={() => naviagte('/store/5')}>
+            <CategoryIcon>eco</CategoryIcon>
+            <CategoryItemText>배경</CategoryItemText>
+            <Move />
+          </CategoryItem>
+        </CategoryList>
+      </Layout>
       <Footer />
-    </Layout>
+    </>
   );
 };
 
@@ -56,6 +66,7 @@ const Layout = styled.div`
   position: relative;
   width: 100vw;
   height: 100vh;
+  padding-top: 1rem;
   background-image: url(${ScreenBackground});
 `;
 
@@ -74,14 +85,13 @@ const PointContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: end;
-  padding-right: 1rem;
-  border-style: solid;
+  justify-content: space-between;
+  padding-right: 0.5rem;
+  border: solid 1.2px;
   border-radius: 1.5rem;
   border-color: ${(props) => props.theme.colors.orange};
+  background: 1rem;
   background-color: ${(props) => props.theme.colors.lightOrange};
-  font-size: 20px;
-  font-weight: bold;
 `;
 
 const Icon = styled.button`
@@ -96,17 +106,15 @@ const Icon = styled.button`
 
 const Point = styled.div`
   display: flex;
-  padding-left: 0.9rem;
-  font-family: 'SUITE-Bold';
-  font-size: 20px;
-  font-weight: bold;
   flex-direction: row;
   align-items: center;
   justify-content: end;
+  font-family: 'SUITE-Bold', sans-serif;
+  font-size: 1.3rem;
 `;
 
 const IconWrapper = styled.div`
-  font-family: 'Material Symbols Outlined', sans-serif;
+  border-radius: 50%;
   color: ${(props) => props.theme.colors.orange};
   cursor: default;
 `;
@@ -137,13 +145,11 @@ const CategoryItemText = styled.div`
   font-size: 18px;
 `;
 
-const ClothIcon = styled.div`
+const CategoryIcon = styled.div`
   margin-left: 20px;
   font-family: 'Material Symbols Outlined';
   font-size: 30px;
-  &:before {
-    content: 'apparel';
-  }
+  color: white;
 `;
 
 const Move = styled.div`
@@ -153,42 +159,6 @@ const Move = styled.div`
   font-size: 30px;
   &:before {
     content: 'east';
-  }
-`;
-
-const HangerIcon = styled.div`
-  margin-left: 20px;
-  font-family: 'Material Symbols Outlined';
-  font-size: 30px;
-  &:before {
-    content: 'checkroom';
-  }
-`;
-
-const FaceIcon = styled.div`
-  margin-left: 20px;
-  font-family: 'Material Symbols Outlined';
-  font-size: 30px;
-  &:before {
-    content: 'face';
-  }
-`;
-
-const FloorIcon = styled.div`
-  margin-left: 20px;
-  font-family: 'Material Symbols Outlined';
-  font-size: 30px;
-  &:before {
-    content: 'grid_on';
-  }
-`;
-
-const WallIcon = styled.div`
-  margin-left: 20px;
-  font-family: 'Material Symbols Outlined';
-  font-size: 30px;
-  &:before {
-    content: 'settings_system_daydream';
   }
 `;
 
